d39f48a53a58fe9d5d3b9bb80d7a6bd8f26218e9
soypat <graded.sp@gmail.com>
2023-12-12 00:01:52 -0300

    remodel ARP client implementation

--- stacks/arp.go
+++ stacks/arp.go
@@ -3,35 +3,15 @@ package stacks
 import (
 	"errors"
 	"log/slog"
+	"net/netip"
 
 	"github.com/soypat/seqs/eth"
 )
 
-func (ps *PortStack) BeginResolveARPv4(target [4]byte) {
-	ps.arpResult = eth.ARPv4Header{
-		Operation:      1, // Request.
-		HardwareType:   1, // Ethernet.
-		ProtoType:      uint16(eth.EtherTypeIPv4),
-		HardwareLength: 6,
-		ProtoLength:    4,
-		HardwareSender: ps.MACAs6(),
-		ProtoSender:    ps.ip,
-		HardwareTarget: [6]byte{}, // Zeroes, is filled by target.
-		ProtoTarget:    target,
-	}
-}
-
-// ARPv4Result returns the result of the last ARPv4 request.
-func (ps *PortStack) ARPv4Result() (eth.ARPv4Header, bool) {
-	return ps.arpResult, ps.arpResult.Operation == 2
-}
-
-func (ps *PortStack) pendingReplyToARP() bool {
-	return ps.pendingARPresponse.Operation == 2 // 2 means reply.
-}
-
-func (ps *PortStack) pendingResolveARPv4() bool {
-	return ps.arpResult.Operation == 1 // User asked for a ARP request.
+// ARP returns the ARP client for this stack. The type is not exported since
+// it's implementation is experimental.
+func (ps *PortStack) ARP() *arpClient {
+	return &ps.arpClient
 }
 
 /*
@@ -52,75 +32,116 @@ PortStack.pendingARPresponse contains state:
 1. Upon ARP request in `recvARP`, case 1: Store outgoing ARP response in PortStack.pendingARPresponse. PortStack.pendingARPresponse.Operation = 2 (reply)
 2. Upon `handleARP`: Packet sent out. PortStack.pendingARPresponse.Operation = 0 (no pending response) Ready to receive.
 */
+type arpClient struct {
+	stack           *PortStack
+	result          eth.ARPv4Header
+	pendingResponse eth.ARPv4Header
+}
 
-func (ps *PortStack) handleARP(dst []byte) (n int) {
-	pendingResolve := ps.pendingResolveARPv4()
+func (c *arpClient) ResultAs6() (netip.Addr, [6]byte, error) {
+	switch c.result.Operation {
+	case 0:
+		return netip.Addr{}, [6]byte{}, errors.New("no ARP in progress")
+	case 1:
+		return netip.Addr{}, [6]byte{}, errors.New("ARP response pending")
+	}
+	return netip.AddrFrom4(c.result.ProtoSender), c.result.HardwareSender, nil
+}
+
+func (c *arpClient) BeginResolve(addr netip.Addr) error {
+	if !addr.Is4() {
+		return errors.New("ARP only supports IPv4")
+	}
+	c.result = eth.ARPv4Header{
+		Operation:      1, // Request.
+		HardwareType:   1, // Ethernet.
+		ProtoType:      uint16(eth.EtherTypeIPv4),
+		HardwareLength: 6,
+		ProtoLength:    4,
+		HardwareSender: c.stack.MACAs6(),
+		ProtoSender:    c.stack.ip,
+		HardwareTarget: [6]byte{}, // Zeroes, is filled by target.
+		ProtoTarget:    addr.As4(),
+	}
+	return nil
+}
+
+func (c *arpClient) isPending() bool {
+	return c.pendingReplyToARP() || c.pendingResolveARPv4()
+}
+
+func (c *arpClient) pendingReplyToARP() bool {
+	return c.pendingResponse.Operation == 2 // 2 means reply.
+}
+
+func (c *arpClient) pendingResolveARPv4() bool {
+	return c.result.Operation == 1 // User asked for a ARP request.
+}
+
+func (c *arpClient) handle(dst []byte) (n int) {
+	pendingResolve := c.pendingResolveARPv4()
 	switch {
 	case pendingResolve:
 		// We have a pending request from user to perform ARP.
 		ehdr := eth.EthernetHeader{
 			Destination:     eth.BroadcastHW6(),
-			Source:          ps.MACAs6(),
+			Source:          c.stack.MACAs6(),
 			SizeOrEtherType: uint16(eth.EtherTypeARP),
 		}
 		ehdr.Put(dst)
-		ps.arpResult.Put(dst[eth.SizeEthernetHeader:])
-		ps.arpResult.Operation = arpOpWait // Clear pending ARP to not loop.
+		c.result.Put(dst[eth.SizeEthernetHeader:])
+		c.result.Operation = arpOpWait // Clear pending ARP to not loop.
 		n = eth.SizeEthernetHeader + eth.SizeARPv4Header
 
-	case ps.pendingReplyToARP():
+	case c.pendingReplyToARP():
 		// We need to respond to an ARP request that queries our address.
 		ehdr := eth.EthernetHeader{
-			Destination:     ps.pendingARPresponse.HardwareTarget,
-			Source:          ps.MACAs6(),
+			Destination:     c.pendingResponse.HardwareTarget,
+			Source:          c.stack.MACAs6(),
 			SizeOrEtherType: uint16(eth.EtherTypeARP),
 		}
 		ehdr.Put(dst)
-		ps.pendingARPresponse.Put(dst[eth.SizeEthernetHeader:])
-		ps.pendingARPresponse.Operation = 0 // Clear pending ARP.
+		c.pendingResponse.Put(dst[eth.SizeEthernetHeader:])
+		c.pendingResponse.Operation = 0 // Clear pending ARP.
 		n = eth.SizeEthernetHeader + eth.SizeARPv4Header
 
 	default:
 		// return 0 // Nothing to do, n=0.
 	}
 	if n > 0 {
-		ps.debug("ARP:send", slog.Bool("isReply", !pendingResolve))
+		c.stack.debug("ARP:send", slog.Bool("isReply", !pendingResolve))
 	}
 	return n
 }
 
-func (ps *PortStack) recvARP(ethPayload []byte) error {
-	if len(ethPayload) < eth.SizeARPv4Header {
-		return errors.New("short ARP payload")
-	}
-	ahdr := eth.DecodeARPv4Header(ethPayload)
+func (c *arpClient) recv(ahdr *eth.ARPv4Header) error {
 	if ahdr.HardwareLength != 6 || ahdr.ProtoLength != 4 || ahdr.HardwareType != 1 || ahdr.AssertEtherType() != eth.EtherTypeIPv4 {
 		return errors.New("unsupported ARP") // Ignore ARP unsupported requests.
 	}
 	switch ahdr.Operation {
 	case 1: // We received ARP request.
-		if ps.pendingReplyToARP() || ahdr.ProtoTarget != ps.ip {
+		if c.pendingReplyToARP() || ahdr.ProtoTarget != c.stack.ip {
 			return nil // ARP reply pending or not for us.
 		}
 		// We need to respond to this ARP request by inverting Sender/Target fields.
 		ahdr.HardwareTarget = ahdr.HardwareSender
 		ahdr.ProtoTarget = ahdr.ProtoSender
 
-		ahdr.HardwareSender = ps.MACAs6()
-		ahdr.ProtoSender = ps.ip
+		ahdr.HardwareSender = c.stack.MACAs6()
+		ahdr.ProtoSender = c.stack.ip
 		ahdr.Operation = 2 // Set as reply. This also flags the packet as pending.
-		ps.pendingARPresponse = ahdr
+		c.pendingResponse = *ahdr
 
 	case 2: // We received ARP reply.
-		if ps.arpResult.Operation != arpOpWait || // Result already received
-			ahdr.ProtoTarget != ps.ip || // Not meant for us.
-			ahdr.ProtoSender != ps.arpResult.ProtoTarget { // does not correspond to last request.
+		if c.result.Operation != arpOpWait || // Result already received
+			ahdr.ProtoTarget != c.stack.ip || // Not meant for us.
+			ahdr.ProtoSender != c.result.ProtoTarget { // does not correspond to last request.
 			return nil
 		}
-		ps.arpResult = ahdr
+		c.result = *ahdr
 	default:
 		return errors.New("unsupported ARP operation")
 	}
-	ps.debug("ARP:recv", slog.Int("op", int(ahdr.Operation)))
+	c.stack.debug("ARP:recv", slog.Int("op", int(ahdr.Operation)))
 	return nil
 }
