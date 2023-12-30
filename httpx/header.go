3c4e0f80795d904130ab1bb5edf55dd806c26259
soypat <graded.sp@gmail.com>
2023-12-29 22:51:40 -0300

    add more logic and tests

--- httpx/header.go
+++ httpx/header.go
@@ -1,6 +1,7 @@
 package httpx
 
 import (
+	"bytes"
 	"errors"
 	"net/http"
 	"strconv"
@@ -22,6 +23,7 @@ type header struct {
 	cookies              []argsKV
 	disableNormalizing   bool
 	disableSpecialHeader bool
+	noDefaultContentType bool
 	connectionClose      bool
 	noHTTP11             bool
 	cookiesCollected     bool
@@ -196,3 +198,198 @@ func isBadTrailer(key string) bool {
 	}
 	return false
 }
+
+// RawHeaders returns raw header key/value bytes.
+//
+// Depending on server configuration, header keys may be normalized to
+// capital-case in place.
+//
+// This copy is set aside during parsing, so empty slice is returned for all
+// cases where parsing did not happen. Similarly, request line is not stored
+// during parsing and can not be returned.
+//
+// The slice is not safe to use after the handler returns.
+func (h *header) RawHeaders() []byte {
+	return h.rawHeaders
+}
+
+// DisableNormalizing disables header names' normalization.
+//
+// By default all the header names are normalized by uppercasing
+// the first letter and all the first letters following dashes,
+// while lowercasing all the other letters.
+// Examples:
+//
+//   - CONNECTION -> Connection
+//   - conteNT-tYPE -> Content-Type
+//   - foo-bar-baz -> Foo-Bar-Baz
+//
+// Disable header names' normalization only if know what are you doing.
+func (h *header) DisableNormalizing() {
+	h.disableNormalizing = true
+}
+
+// DisableSpecialHeader disables special header processing.
+// fasthttp will not set any special headers for you, such as Host, Content-Type, User-Agent, etc.
+// You must set everything yourself.
+// If RequestHeader.Read() is called, special headers will be ignored.
+// This can be used to control case and order of special headers.
+// This is generally not recommended.
+func (h *header) DisableSpecialHeader() {
+	h.disableSpecialHeader = true
+}
+
+// String returns request header representation.
+func (h *header) String() string {
+	return string(h.Header())
+}
+
+// Header returns request header representation.
+//
+// Headers that set as Trailer will not represent. Use TrailerHeader for trailers.
+//
+// The returned value is valid until the request is released,
+// either though ReleaseRequest or your request handler returning.
+// Do not store references to returned value. Make copies instead.
+func (h *header) Header() []byte {
+	h.bufKV.value = h.AppendBytes(h.bufKV.value[:0])
+	return h.bufKV.value
+}
+
+// Method returns HTTP request method.
+func (h *header) Method() []byte {
+	if len(h.method) == 0 {
+		h.method = append(h.method, http.MethodGet...)
+	}
+	return h.method
+}
+
+// RequestURI returns RequestURI from the first HTTP request line.
+func (h *header) RequestURI() []byte {
+	requestURI := h.requestURI
+	if len(requestURI) == 0 {
+		requestURI = append(requestURI, '/')
+	}
+	return requestURI
+}
+
+// Protocol returns HTTP protocol.
+func (h *header) Protocol() []byte {
+	if len(h.proto) == 0 {
+		h.proto = append(h.proto, strHTTP11...)
+	}
+	return h.proto
+}
+
+// AppendBytes appends request header representation to dst and returns
+// the extended dst.
+func (h *header) AppendBytes(dst []byte) []byte {
+	dst = append(dst, h.Method()...)
+	dst = append(dst, ' ')
+	dst = append(dst, h.RequestURI()...)
+	dst = append(dst, ' ')
+	dst = append(dst, h.Protocol()...)
+	dst = append(dst, strCRLF...)
+
+	userAgent := h.UserAgent()
+	if len(userAgent) > 0 && !h.disableSpecialHeader {
+		dst = appendHeaderLine(dst, strUserAgent, b2s(userAgent))
+	}
+
+	host := h.Host()
+	if len(host) > 0 && !h.disableSpecialHeader {
+		dst = appendHeaderLine(dst, strHost, b2s(host))
+	}
+
+	contentType := h.ContentType()
+	if !h.noDefaultContentType && len(contentType) == 0 && !h.ignoreBody() {
+		contentType = append(contentType, strDefaultContentType...)
+	}
+	if len(contentType) > 0 && !h.disableSpecialHeader {
+		dst = appendHeaderLine(dst, strContentType, b2s(contentType))
+	}
+	if len(h.contentLengthBytes) > 0 && !h.disableSpecialHeader {
+		dst = appendHeaderLine(dst, strContentLength, b2s(h.contentLengthBytes))
+	}
+
+	for i, n := 0, len(h.h); i < n; i++ {
+		kv := &h.h[i]
+		// Exclude trailer from header
+		exclude := false
+		for _, t := range h.trailer {
+			if bytes.Equal(kv.key, t.key) {
+				exclude = true
+				break
+			}
+		}
+		if !exclude {
+			dst = appendHeaderLine(dst, b2s(kv.key), b2s(kv.value))
+		}
+	}
+
+	if len(h.trailer) > 0 {
+		aux := appendArgsKey(nil, h.trailer, strCommaSpace)
+		dst = appendHeaderLine(dst, strTrailer, b2s(aux))
+	}
+
+	// there is no need in h.collectCookies() here, since if cookies aren't collected yet,
+	// they all are located in h.h.
+	n := len(h.cookies)
+	if n > 0 && !h.disableSpecialHeader {
+		dst = append(dst, strCookie...)
+		dst = append(dst, strColonSpace...)
+		dst = appendRequestCookieBytes(dst, h.cookies)
+		dst = append(dst, strCRLF...)
+	}
+
+	if h.ConnectionClose() && !h.disableSpecialHeader {
+		dst = appendHeaderLine(dst, strConnection, strClose)
+	}
+
+	return append(dst, strCRLF...)
+}
+
+func appendHeaderLine(dst []byte, key, value string) []byte {
+	dst = append(dst, key...)
+	dst = append(dst, strColonSpace...)
+	dst = append(dst, value...)
+	return append(dst, strCRLF...)
+}
+
+func (h *header) ignoreBody() bool {
+	return h.IsGet() || h.IsHead()
+}
+
+func (h *header) MethodIs(method string) bool {
+	return b2s(h.method) == method
+}
+
+// IsGet returns true if request method is GET.
+func (h *header) IsGet() bool { return len(h.method) == 0 || h.MethodIs(http.MethodGet) }
+
+// IsHead returns true if request method is HEAD.
+func (h *header) IsHead() bool { return h.MethodIs(http.MethodHead) }
+
+// IsPost returns true if request method is POST.
+func (h *header) IsPost() bool { return h.MethodIs(http.MethodPost) }
+
+// IsPut returns true if request method is PUT.
+func (h *header) IsPut() bool { return h.MethodIs(http.MethodPut) }
+
+// IsDelete returns true if request method is DELETE.
+func (h *header) IsDelete() bool { return h.MethodIs(http.MethodDelete) }
+
+// IsConnect returns true if request method is CONNECT.
+func (h *header) IsConnect() bool { return h.MethodIs(http.MethodConnect) }
+
+// IsOptions returns true if request method is OPTIONS.
+func (h *header) IsOptions() bool { return h.MethodIs(http.MethodOptions) }
+
+// IsTrace returns true if request method is TRACE.
+func (h *header) IsTrace() bool { return h.MethodIs(http.MethodTrace) }
+
+// IsPatch returns true if request method is PATCH.
+func (h *header) IsPatch() bool { return h.MethodIs(http.MethodPatch) }
+
+// IsHTTP11 returns true if the request is HTTP/1.1.
+func (h *header) IsHTTP11() bool { return !h.noHTTP11 }
--- httpx/header_test.go
+++ httpx/header_test.go
@@ -3,6 +3,7 @@ package httpx
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"testing"
 )
 
@@ -30,4 +31,126 @@ func TestRequestHeaderEmptyValueFromString(t *testing.T) {
 	if len(v2) > 0 {
 		t.Fatalf("expecting empty value. Got %q", v2)
 	}
+}
+
+func TestRequestRawHeaders(t *testing.T) {
+	t.Parallel()
+
+	kvs := "hOsT: foobar\r\n" +
+		"value:  b\r\n" +
+		"\r\n"
+	t.Run("normalized", func(t *testing.T) {
+		s := "GET / HTTP/1.1\r\n" + kvs
+		exp := kvs
+		var h header
+		br := bufio.NewReader(bytes.NewBufferString(s))
+		if err := h.Read(br); err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		if string(h.Host()) != "foobar" {
+			t.Fatalf("unexpected host: %q. Expecting %q", h.Host(), "foobar")
+		}
+		v2 := h.Peek("Value")
+		if !bytes.Equal(v2, []byte{'b'}) {
+			t.Fatalf("expecting non empty value. Got %q", v2)
+		}
+		if raw := h.RawHeaders(); string(raw) != exp {
+			t.Fatalf("expected header %q, got %q", exp, raw)
+		}
+	})
+	for _, n := range []int{0, 1, 4, 8} {
+		t.Run(fmt.Sprintf("post-%dk", n), func(t *testing.T) {
+			l := 1024 * n
+			body := make([]byte, l)
+			for i := range body {
+				body[i] = 'a'
+			}
+			cl := fmt.Sprintf("Content-Length: %d\r\n", l)
+			s := "POST / HTTP/1.1\r\n" + cl + kvs + string(body)
+			exp := cl + kvs
+			var h header
+			br := bufio.NewReader(bytes.NewBufferString(s))
+			if err := h.Read(br); err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if string(h.Host()) != "foobar" {
+				t.Fatalf("unexpected host: %q. Expecting %q", h.Host(), "foobar")
+			}
+			v2 := h.Peek("Value")
+			if !bytes.Equal(v2, []byte{'b'}) {
+				t.Fatalf("expecting non empty value. Got %q", v2)
+			}
+			if raw := h.RawHeaders(); string(raw) != exp {
+				t.Fatalf("expected header %q, got %q", exp, raw)
+			}
+		})
+	}
+	t.Run("http10", func(t *testing.T) {
+		s := "GET / HTTP/1.0\r\n" + kvs
+		exp := kvs
+		var h header
+		br := bufio.NewReader(bytes.NewBufferString(s))
+		if err := h.Read(br); err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		if string(h.Host()) != "foobar" {
+			t.Fatalf("unexpected host: %q. Expecting %q", h.Host(), "foobar")
+		}
+		v2 := h.Peek("Value")
+		if !bytes.Equal(v2, []byte{'b'}) {
+			t.Fatalf("expecting non empty value. Got %q", v2)
+		}
+		if raw := h.RawHeaders(); string(raw) != exp {
+			t.Fatalf("expected header %q, got %q", exp, raw)
+		}
+	})
+	t.Run("no-kvs", func(t *testing.T) {
+		s := "GET / HTTP/1.1\r\n\r\n"
+		exp := ""
+		var h header
+		h.DisableNormalizing()
+		br := bufio.NewReader(bytes.NewBufferString(s))
+		if err := h.Read(br); err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		if string(h.Host()) != "" {
+			t.Fatalf("unexpected host: %q. Expecting %q", h.Host(), "")
+		}
+		v1 := h.Peek("NoKey")
+		if len(v1) > 0 {
+			t.Fatalf("expecting empty value. Got %q", v1)
+		}
+		if raw := h.RawHeaders(); string(raw) != exp {
+			t.Fatalf("expected header %q, got %q", exp, raw)
+		}
+	})
+}
+
+func TestRequestDisableSpecialHeaders(t *testing.T) {
+	t.Parallel()
+
+	kvs := "Host: foobar\r\n" +
+		"User-Agent: ua\r\n" +
+		"Non-Special: val\r\n" +
+		"\r\n"
+
+	var h header
+	h.DisableSpecialHeader()
+
+	s := "GET / HTTP/1.0\r\n" + kvs
+	br := bufio.NewReader(bytes.NewBufferString(s))
+	if err := h.Read(br); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	// assert order of all headers preserved
+	if h.String() != s {
+		t.Fatalf("Headers not equal: %q. Expecting %q", h.String(), s)
+	}
+	// h.SetCanonical([]byte("host"), []byte("notfoobar"))
+	// if string(h.Host()) != "foobar" {
+	// 	t.Fatalf("unexpected: %q. Expecting %q", h.Host(), "foobar")
+	// }
+	// if h.String() != "GET / HTTP/1.0\r\nHost: foobar\r\nUser-Agent: ua\r\nNon-Special: val\r\nhost: notfoobar\r\n\r\n" {
+	// 	t.Fatalf("custom special header ordering failed: %q", h.String())
+	// }
 }
