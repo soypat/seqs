a8a402bfe3efce2733086f814ec3efce469ad632
soypat <graded.sp@gmail.com>
2024-05-14 13:49:52 -0300

    SCB method returns pointer

--- stacks/intern_test.go
+++ stacks/intern_test.go
@@ -251,7 +251,7 @@ func setRingData(t *testing.T, r *ring, offset int, data []byte) {
 
 // SCB is an internal routine for testing which returns the control block,
 // which is a simplified implementation of the TCB of RFC9293.
-func (tcp *TCPConn) SCB() seqs.ControlBlock { return tcp.scb }
+func (tcp *TCPConn) SCB() *seqs.ControlBlock { return &tcp.scb }
 
 func (dhcpc *DHCPClient) PortStack() *PortStack { return dhcpc.stack }
 func (dhcps *DHCPServer) PortStack() *PortStack { return dhcps.stack }
