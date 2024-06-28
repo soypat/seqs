255f08deee192dcb9cf1ad264dcccbb3768739a3
soypat <graded.sp@gmail.com>
2024-06-28 19:50:39 -0300

    fix RingBuffers return type

--- stacks/intern_test.go
+++ stacks/intern_test.go
@@ -2,6 +2,7 @@ package stacks
 
 import (
 	"github.com/soypat/seqs"
+	"github.com/soypat/seqs/internal"
 )
 
 // SCB is an internal routine for testing which returns the control block,
@@ -11,6 +12,6 @@ func (tcp *TCPConn) SCB() *seqs.ControlBlock { return &tcp.scb }
 func (dhcpc *DHCPClient) PortStack() *PortStack { return dhcpc.stack }
 func (dhcps *DHCPServer) PortStack() *PortStack { return dhcps.stack }
 
-func (tcp *TCPConn) RingBuffers() (rx, tx *ring) {
+func (tcp *TCPConn) RingBuffers() (rx, tx *internal.Ring) {
 	return &tcp.rx, &tcp.tx
 }
